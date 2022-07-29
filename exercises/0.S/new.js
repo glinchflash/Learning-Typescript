86cb7bca165903891d2ceef3871a76a9e64d3058
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-07-29 17:03:38 +0200

    fixed bug in setup

--- exercises/0.S/new.js
+++ exercises/0.S/new.js
@@ -1,23 +1,16 @@
-var Car = /** @class */ (function () {
-    function Car(MAXIMUM_FUEL_CAPACITY) {
+"use strict";
+class Car {
+    constructor(MAXIMUM_FUEL_CAPACITY) {
         this._engine = new Engine(MAXIMUM_FUEL_CAPACITY);
         this._radio = new Radio();
     }
-    Object.defineProperty(Car.prototype, "engine", {
-        get: function () {
-            return this._engine;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Car.prototype, "radio", {
-        get: function () {
-            return this._radio;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Car.prototype.drive = function () {
+    get engine() {
+        return this._engine;
+    }
+    get radio() {
+        return this._radio;
+    }
+    drive() {
         if (this._engine.Status === false || this._engine.fuel <= 0) {
             //what I am doing here is a good principle called "failing early"
             // If you have some conditions you need to check, that will exclude most of the code in your function check that first
@@ -26,97 +19,78 @@ var Car = /** @class */ (function () {
         }
         this._engine.consumeFuel();
         this._engine.addMileage();
-    };
-    return Car;
-}());
-var Radio = /** @class */ (function () {
-    function Radio() {
+    }
+}
+class Radio {
+    constructor() {
         this._musicLevel = 0;
         this._oldMusicLevel = 50;
     }
-    Object.defineProperty(Radio.prototype, "musicLevel", {
-        //Take attention to these getter and setters
-        get: function () {
-            return this._musicLevel;
-        },
-        set: function (value) {
-            this._musicLevel = value;
-            this._oldMusicLevel = value;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Radio.prototype.turnMusicOn = function () {
+    //Take attention to these getter and setters
+    get musicLevel() {
+        return this._musicLevel;
+    }
+    set musicLevel(value) {
+        this._musicLevel = value;
+        this._oldMusicLevel = value;
+    }
+    turnMusicOn() {
         this._musicLevel = this._oldMusicLevel;
-    };
-    Radio.prototype.turnMusicOff = function () {
+    }
+    turnMusicOff() {
         this._musicLevel = 0;
-    };
-    return Radio;
-}());
-var Engine = /** @class */ (function () {
-    function Engine(MAXIMUM_FUEL_CAPACITY) {
+    }
+}
+class Engine {
+    constructor(MAXIMUM_FUEL_CAPACITY) {
         this._fuel = 0;
         this._miles = 0;
         this._Status = false;
         this.FUEL_MILEAGE = 10;
         this.MAXIMUM_FUEL_CAPACITY = MAXIMUM_FUEL_CAPACITY;
     }
-    Object.defineProperty(Engine.prototype, "miles", {
-        get: function () {
-            return this._miles;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Object.defineProperty(Engine.prototype, "fuel", {
-        get: function () {
-            return this._fuel;
-        },
-        enumerable: false,
-        configurable: true
-    });
+    get miles() {
+        return this._miles;
+    }
+    get fuel() {
+        return this._fuel;
+    }
     //When a value can only go one way (you add fuel, consuming fuel is handled by the car itself)
     // it is better to provide a specific method for this instead of a generic setter.
     // with a setter there is always the chance of somebody lowering the fuel amount by accident.
-    Engine.prototype.addFuel = function (fuel) {
+    addFuel(fuel) {
         this._fuel = Math.min(fuel + this._fuel, this.MAXIMUM_FUEL_CAPACITY);
-    };
-    Object.defineProperty(Engine.prototype, "Status", {
-        get: function () {
-            return this._Status;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    Engine.prototype.turnEngineOn = function () {
+    }
+    get Status() {
+        return this._Status;
+    }
+    turnEngineOn() {
         this._Status = true;
-    };
-    Engine.prototype.turnEngineOff = function () {
+    }
+    turnEngineOff() {
         this._Status = false;
-    };
-    Engine.prototype.consumeFuel = function () {
+    }
+    consumeFuel() {
         this._fuel -= 1;
-    };
-    Engine.prototype.addMileage = function () {
+    }
+    addMileage() {
         this._miles += this.FUEL_MILEAGE;
-    };
-    return Engine;
-}());
+    }
+}
 // When you see <cast>variable this is a "cast" of a variable, explicitly telling the code what the type of this variable will be.
 // This is sometimes needed when a default JS function does not return a precise enough Type.
 // I need to cast this to HtmlElement because the default Element return type is not specific to the HTML context (because some versions of JS can also be used in the backend, see node.js)
 // This makes it not having some properties like .innerText. Test it out yourself by removing the <HTMLElement>
-var musicToggleElement = document.querySelector('#music-toggle');
-var musicSliderElement = document.querySelector('#music-slider');
-var engineToggleElement = document.querySelector('#engine-toggle');
-var addFuelForm = document.querySelector('#add-fuel-form');
-var addFuelInput = document.querySelector('#add-fuel-input');
-var fuelLevelElement = document.querySelector('#fuel-level');
-var milesElement = document.querySelector('#miles-value');
-var audioElement = document.querySelector('#car-music');
-var car = new Car(100);
-musicToggleElement.addEventListener('click', function () {
+const musicToggleElement = document.querySelector('#music-toggle');
+const musicSliderElement = document.querySelector('#music-slider');
+const engineToggleElement = document.querySelector('#engine-toggle');
+const addFuelForm = document.querySelector('#add-fuel-form');
+const addFuelInput = document.querySelector('#add-fuel-input');
+const fuelLevelElement = document.querySelector('#fuel-level');
+const milesElement = document.querySelector('#miles-value');
+const audioElement = document.querySelector('#car-music');
+let car = new Car(100);
+musicToggleElement.addEventListener('click', () => {
     if (car.radio.musicLevel === 0) {
         car.radio.turnMusicOn();
         musicSliderElement.value = car.radio.musicLevel.toString();
@@ -127,14 +101,14 @@ musicToggleElement.addEventListener('click', function () {
     car.radio.turnMusicOff();
 });
 //I use input instead of change, because then the value changes when I move the mouse, not only on release
-musicSliderElement.addEventListener('input', function (event) {
-    var target = (event.target);
+musicSliderElement.addEventListener('input', (event) => {
+    let target = (event.target);
     car.radio.musicLevel = target.value;
     audioElement.volume = car.radio.musicLevel / 100;
     //@todo when you are repeating the same text over and over again maybe we should have made some constants for it? Can you do improve on this?
     musicToggleElement.innerText = car.radio.musicLevel ? 'Turn music off' : 'Turn music on';
 });
-engineToggleElement.addEventListener('click', function () {
+engineToggleElement.addEventListener('click', () => {
     if (car.engine.Status) {
         car.engine.turnEngineOff();
         engineToggleElement.innerText = 'Turn engine on';
@@ -143,12 +117,12 @@ engineToggleElement.addEventListener('click', function () {
     engineToggleElement.innerText = 'Turn engine off';
     car.engine.turnEngineOn();
 });
-addFuelForm.addEventListener('submit', function (event) {
+addFuelForm.addEventListener('submit', (event) => {
     event.preventDefault();
     car.engine.addFuel(Number(addFuelInput.value));
     fuelLevelElement.innerText = car.engine.fuel.toString();
 });
-setInterval(function () {
+setInterval(() => {
     car.drive();
     //while it looks like both lines below are the same there is a subtle difference (you could put breakpoints here to see the difference):
     // this <cast> will only tell TypeScript that the value is a string, but the actual variable in JS is not changed in any way: it is in reality still a number
