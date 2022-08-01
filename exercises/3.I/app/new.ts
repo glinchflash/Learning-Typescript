0f6dfaa5bede893088d35e1de8182c296b178bc6
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-01 17:01:23 +0200

    new way of defining what type of user is logging in + error handling --> only have to solve how to define google bot + error handling

--- exercises/3.I/app/new.ts
+++ exercises/3.I/app/new.ts
@@ -1,6 +1,6 @@
 import {User} from "./models/classes/User";
 import {Admin} from "./models/classes/Admin";
-import {GoogleBot} from "./models/classes/Google";
+import {GoogleBot} from "./models/classes/GoogleBot";
 
 const passwordElement = <HTMLInputElement>document.querySelector('#password');
 const typePasswordElement = <HTMLInputElement>document.querySelector('#typePassword');
@@ -9,48 +9,48 @@ const typeFacebookElement = <HTMLInputElement>document.querySelector('#typeFaceb
 const loginAsAdminElement = <HTMLInputElement>document.querySelector('#loginAsAdmin');
 const resetPasswordElement = <HTMLAnchorElement>document.querySelector('#resetPassword');
 
-let guest = new User('fb', 'google');
-let admin = new Admin;
-let bot = new GoogleBot('google', 'google')
-
 
 document.querySelector('#login-form')!.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    let user;
-    if (loginAsAdminElement.checked){
-        user = admin;
-        console.log(user);
-    } else if (!loginAsAdminElement.checked && passwordElement.value !== 'google'){
-        user = guest;
-        console.log(user);
-    } else if (!loginAsAdminElement.checked && passwordElement.value === 'google'){
-        user = bot;
-        console.log(user);
-    }
-
-    if (user === guest) {
-        user.setGoogleToken('secret_token_google');
-        user.setFacebookToken('secret_token_fb');
-    } else if (user === bot) {
-        user.setGoogleToken('secret_token_google');
-    }
-
     debugger;
 
+    let user;
     let auth = false;
     switch (true) {
-        case typePasswordElement.checked:
+        case (loginAsAdminElement.checked && typePasswordElement.checked):
+            user = new Admin();
             auth = user.checkPassword(passwordElement.value);
+            console.log(user);
             break;
-        case typeGoogleElement.checked:
+        case (loginAsAdminElement.checked && !typePasswordElement.checked):
+            alert('Admins can only sign in through Password');
+            break;
+        case (!loginAsAdminElement.checked || passwordElement.value === "user"):
+            user = new User();
+            user.setGoogleToken('secret_token_google');
+            user.setFacebookToken('secret_token_fb');
+            switch (true) {
+                case typePasswordElement.checked:
+                    auth = user.checkPassword(passwordElement.value);
+                    break;
+                case  typeFacebookElement.checked:
+                    auth = user.getFacebookLogin('secret_token_fb');
+                    break
+                case typeGoogleElement.checked:
+                    auth = user.checkGoogleLogin('secret_token_google');
+                    break;
+            }
+            console.log(user);
+            break;
+        case (!loginAsAdminElement.checked && typeGoogleElement.checked && passwordElement.value === ""):
+            user = new GoogleBot()
+            user.setGoogleToken('secret_token_google');
             auth = user.checkGoogleLogin('secret_token_google');
-            break;
-        case typeFacebookElement.checked:
-            debugger;
-            auth = user.getFacebookLogin('secret_token_fb');
+            console.log(user);
             break;
     }
+
 
     if (auth) {
         alert('login success');
@@ -62,6 +62,7 @@ document.querySelector('#login-form')!.addEventListener('submit', (event) => {
 resetPasswordElement.addEventListener('click', (event) => {
     event.preventDefault();
 
-    let user = loginAsAdminElement.checked ? admin : guest;
+    let user = loginAsAdminElement.checked ? new Admin() : new User();
     user.resetPassword();
+
 });
