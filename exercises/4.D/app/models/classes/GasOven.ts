106afffa1e9d0dc7b1253878413d08b163dd9f90
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-03 10:08:32 +0200

    added access modifiers

--- exercises/4.D/app/models/classes/GasOven.ts
+++ exercises/4.D/app/models/classes/GasOven.ts
@@ -1,43 +1,42 @@
 import {CookingDevices} from "../iCookingDevices";
-export class GasOven implements CookingDevices{
-    private _isOn : boolean = false;
 
-    turnOn() {
+export class GasOven implements CookingDevices {
+    private _isOn: boolean = false;
+
+    public turnOn() {
         this.lightGas()
     }
-    turnOff() {
+
+    public turnOff() {
         this.extinguishGas()
     }
-    bake(item: string)    {
-        if(this._isOn) {
-            setTimeout(function (){
-                document.getElementById('target')!.innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : Now baking " + item + " !</p>";
+
+    public bake(item: string) {
+        if (this._isOn) {
+            setTimeout(function () {
+                document.getElementById('target')!.innerHTML += "<p>" + new Date().getHours() + ":" + new Date().getMinutes() + " : Now baking " + item + " !</p>";
             }, 2000);
             console.log("Now baking " + item + "!");
-        }
-        else
-        {
-            setTimeout(function (){
-                document.getElementById('target')!.innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : there is no gas!</p>";
+        } else {
+            setTimeout(function () {
+                document.getElementById('target')!.innerHTML += "<p>" + new Date().getHours() + ":" + new Date().getMinutes() + " : there is no gas!</p>";
             }, 2000);
             console.log("there is no gas!");//insert fart humor here
         }
     }
 
 
-    public lightGas() : void
-    {
-        setTimeout(function (){
-            document.getElementById('target')!.innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : THE GAS IS ON!</p>";
+    public lightGas(): void {
+        setTimeout(function () {
+            document.getElementById('target')!.innerHTML += "<p>" + new Date().getHours() + ":" + new Date().getMinutes() + " : THE GAS IS ON!</p>";
         }, 1000);
         console.log("THE GAS IS ON!"); //These tacos do hit harder then expected
         this._isOn = true;
     }
 
-    public extinguishGas() : void
-    {
-        setTimeout(function (){
-            document.getElementById('target')!.innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : THE GAS IS OFF!</p><hr>";
+    public extinguishGas(): void {
+        setTimeout(function () {
+            document.getElementById('target')!.innerHTML += "<p>" + new Date().getHours() + ":" + new Date().getMinutes() + " : THE GAS IS OFF!</p><hr>";
         }, 3000);
         console.log("THE GAS IS OFF!"); //Maybe i'll grab a burrito next time
         this._isOn = false;
--- exercises/4.D/app/models/classes/Stove.ts
+++ exercises/4.D/app/models/classes/Stove.ts
@@ -1,42 +1,41 @@
 import {CookingDevices} from "../iCookingDevices";
-export class Stove implements CookingDevices{
-    private _isOn : boolean = false;
 
-    turnOn() {
+export class Stove implements CookingDevices {
+    private _isOn: boolean = false;
+
+    public turnOn() {
         this.switchOnElectricity()
     }
-    turnOff() {
+
+    public turnOff() {
         this.shutdownElectricity()
     }
-    bake(item: string)    {
-        if(this._isOn) {
-            setTimeout(function (){
-                document.getElementById('target')!.innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : Now baking " + item + " !</p>";
+
+    public bake(item: string) {
+        if (this._isOn) {
+            setTimeout(function () {
+                document.getElementById('target')!.innerHTML += "<p>" + new Date().getHours() + ":" + new Date().getMinutes() + " : Now baking " + item + " !</p>";
             }, 2000);
             console.log("Now baking " + item + "!");
-        }
-        else
-        {
-            setTimeout(function (){
-                document.getElementById('target')!.innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : there is no gas!</p>";
+        } else {
+            setTimeout(function () {
+                document.getElementById('target')!.innerHTML += "<p>" + new Date().getHours() + ":" + new Date().getMinutes() + " : there is no gas!</p>";
             }, 2000);
             console.log("Electricity down, dammit!");
         }
     }
 
-    public switchOnElectricity() : void
-    {
-        setTimeout(function (){
-            document.getElementById('target')!.innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : Stove switched on!</p>";
+    public switchOnElectricity(): void {
+        setTimeout(function () {
+            document.getElementById('target')!.innerHTML += "<p>" + new Date().getHours() + ":" + new Date().getMinutes() + " : Stove switched on!</p>";
         }, 1000);
         console.log("This pikachu is increasing my power level to over 9000!");
         this._isOn = true;
     }
 
-    public shutdownElectricity() : void
-    {
-        setTimeout(function (){
-            document.getElementById('target')!.innerHTML += "<p>"+new Date().getHours()+":"+new Date().getMinutes()+" : Stove switched off!</p><hr>";
+    public shutdownElectricity(): void {
+        setTimeout(function () {
+            document.getElementById('target')!.innerHTML += "<p>" + new Date().getHours() + ":" + new Date().getMinutes() + " : Stove switched off!</p><hr>";
         }, 3000);
         console.log("The pikachu flew out of the running wheel!");
         this._isOn = false;
